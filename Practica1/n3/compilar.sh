b7d6acaa3c1b844b723a3751d6b33e39ea7e2eb9
Lauragg <lauragogar@correo.ugr.es>
2017-03-14 19:18:39 +0100

    Dividimos el problema segun eficiencias

---
+++ Practica1/n3/compilar.sh
@@ -0,0 +1,27 @@
+mkdir resultados ejecutable 2> /dev/null
+
+for i in *.cpp
+do
+    j=`echo $i | cut -f 1 -d .`
+    g++ -O2 $i -o ejecutable/$j
+
+    echo $j
+
+    #for o in {1 .. 25}
+    #do
+	      echo "" > resultados/$j.dat
+	      k="1000"
+	      while [ $k -le 100000 ]
+	      do
+	          ./ejecutable/$j $k >> resultados/$j.dat
+	          k=$[$k+1000]
+	      done
+	      # k=1000
+	      # for ((k=1000;k<100000;k=k+1000))
+	      # do
+	      #     ./ejecutable/$j $k >> resultados/$j.dat
+	      # done
+
+    #done
+done
+
