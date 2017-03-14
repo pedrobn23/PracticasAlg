0a156008dc98a451a265218aa87ee7a8dc93d78b
Dani <danipozo@correo.ugr.es>
2017-03-14 18:41:00 +0100

    Corrige script

--- Practica1/compilar.sh
+++ Practica1/compilar.sh
@@ -6,22 +6,22 @@ do
     g++ -O2 $i -o ejecutable/$j
 
     echo $j
-    
-    for i in {1 .. 25}
+
+    for o in {1 .. 25}
     do
-	 echo "" > resultados/$j.dat
-	 k="1000"
-	 while [ $k -le 100000 ]
-	 do
-	     ./ejecutable/$j $k >> resultados/$j.dat
-	     k=$[$k+1000]
-	 done
-	# k=1000
-	# for ((k=1000;k<100000;k=k+1000))
-	# do
-	#     ./ejecutable/$j $k >> resultados/$j.dat
-	# done
-	
-    done    
+	      echo "" > resultados/$j.dat
+	      k="1000"
+	      while [ $k -le 100000 ]
+	      do
+	          ./ejecutable/$j $k >> resultados/$j.dat
+	          k=$[$k+1000]
+	      done
+	      # k=1000
+	      # for ((k=1000;k<100000;k=k+1000))
+	      # do
+	      #     ./ejecutable/$j $k >> resultados/$j.dat
+	      # done
+
+    done
 done
 
