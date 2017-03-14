363fe6a6efde46ee4afebe9009f1d54fb40258d0
fjaviersaezm <fjaviersaezm@gmail.com>
2017-03-14 18:52:09 +0100

    Arregla compilafor

--- Practica1/compilarfor.sh
+++ Practica1/compilarfor.sh
@@ -7,7 +7,7 @@ do
 
     echo $j
     
-    for i in {1 .. 25}
+    for o in {1 .. 25}
     do
 	echo "" > resultados/$j.dat
 	#	k=1000
@@ -17,7 +17,7 @@ do
 	  #   k=$k+1000
 	# done
 	
-	 for ((k=1000;k<100000;k=k+1000))
+	 for (( k=1000; k<100000; k=$[$k+1000] ))
 	 do
 	     ./ejecutable/$j $k >> resultados/$j.dat
 	 done
