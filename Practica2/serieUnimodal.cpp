6a8664a86d64a69900c872ecf7b6fdc1e3ac7771
fjaviersaezm <fjaviersaezm@gmail.com>
2017-04-01 11:17:39 +0200

    Añado práctica 2. pongo ficheros para la presentación y la memoria, usando las mismas plantillas que en la anterior

---
+++ Practica2/serieUnimodal.cpp
@@ -0,0 +1,24 @@
+ #include <iostream>
+ #include <vector>
+
+ using namespace std;
+
+ int fuerzaBruta(int* v, int n){
+
+ int i = 0
+
+ for(i; i < n; ++i) {
+     if(v[i] > v[i+1])
+       return i;
+ }
+
+ int divideYVenceras(int* v, int n){
+
+   int k = n/2;
+   if(v[k] > v[k+1]
+}
+
+int main()
+{
+
+}
