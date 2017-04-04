5b15c743dd06411699f724fb7093b1669a2199b9
Lauragg <lauragogar@correo.ugr.es>
2017-04-04 18:42:46 +0200

    Código en C++ recursivo del divide y vencerás

--- Practica2/serieUnimodal.cpp
+++ Practica2/serieUnimodal.cpp
@@ -1,25 +1,40 @@
- #include <iostream>
- #include <vector>
+#include <iostream>
+#include <vector>
 
  using namespace std;
 
- int fuerzaBruta(int* v, int n){
-
- int i = 0
-
- for(i; i < n; ++i)
- {
+/*
+ * Empezamos en i=1 porque sabemos por el enunciado que nunca estará el máximo en la posición 0 
+ * y la ultima posición que comprobamos es la i=n-2 porque tampoco va a estar nunca en la 
+ * posición i=n-1 y en caso que realizase la comprobación con algún vector en dicha posición
+ * daría violación de segmento.
+*/
+int fuerzaBruta(int* v, int n){
+	for(int i=1; i < n-1; ++i){
      if(v[i] > v[i+1])
        return i;
- }
-
- int divideYVenceras(int* v, int n){
-
-   int k = n/2;
-   if(v[k] > v[k+1]
+	}
 }
 
-int main()
-{
+int divideYVenceras(int* v, int n){
+	int k = n/2;
+	int res;
+	if(n==1)
+		return n;
+
+	else if(v[k] > v[k+1] && v[k] < v[k-1]) 
+		return k;
+
+	else if(v[k] > v[k+1]){
+		res=divideYVenceras(v,k);
+		return res;
+
+	}else{
+		res=divideYVenceras(v+k,n-k);
+		return k+res-1;
+	}	
+}
+
+int main(){
 
 }
