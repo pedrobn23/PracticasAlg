8f3cf4771a211778dc8499ee220652890931899f
Lauragg <lauragogar@correo.ugr.es>
2017-04-04 19:44:48 +0200

    Compilando serieUnimodal.cpp

--- Practica2/generaUnimodal.cpp
+++ Practica2/generaUnimodal.cpp
@@ -7,10 +7,9 @@ using namespace std;
 
 //generador de ejemplos para el problema de la serie unimodal de números. Se genera un índice aleatorio entre 1 y n-2, se asigna el mayor entero (n-1) a ese índice, a los índices anteriores a p se le asignan valores en orden creciente (0,1,...,p-1) y a los índices mayores que p se le asignan valores en orden decreciente (n-2, n-1,...,p
 
-double uniforme() //Genera un número uniformemente distribuido en el
-                  //intervalo [0,1) a partir de uno de los generadores
-                  //disponibles en C.
-{
+double uniforme(){ //Genera un número uniformemente distribuido en el
+                   //intervalo [0,1) a partir de uno de los generadores
+                   //disponibles en C.
 double u;
   u = (double) rand();
   u = u/(double)(RAND_MAX+1.0);
--- Practica2/serieUnimodal.cpp
+++ Practica2/serieUnimodal.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <climits>
+#include <cassert>
 
  using namespace std;
 
@@ -18,26 +21,59 @@ int fuerzaBruta(int* v, int n){
 
 int divideYVenceras(int* v, int n){
 	int k = n/2;
-	int res;
 	if(n==1)
-		return n;
+		return 0; //Caso trivial
+	
+	if(n==2)
+		return v[0]>v[1]?0:1; //Entre dos elementos, manda el más pequeño.
 
-	else if(n==2)
-		return v[0]>v[1]?0:1;
+	if(v[k] > v[k+1]){
 
-	else if(v[k] > v[k+1] && v[k] < v[k-1]) 
-		return k;
+		if(v[k] > v[k-1]) 
+			return k;
 
-	else if(v[k] > v[k+1]){
-		res=divideYVenceras(v,k);
-		return res;
+		//else
+		return divideYVenceras(v,k);
 
-	}else{
-		res=divideYVenceras(v+k,n-k);
-		return k+res-1;
-	}	
+	}
+
+	//else
+	return k + 1 + divideYVenceras(v+k+1,n-k-1);
 }
 
-int main(){
+//generador de ejemplos para el problema de la serie unimodal de números. Se genera un índice aleatorio entre 1 y n-2, se asigna el mayor entero (n-1) a ese índice, a los índices anteriores a p se le asignan valores en orden creciente (0,1,...,p-1) y a los índices mayores que p se le asignan valores en orden decreciente (n-2, n-1,...,p
+
+double uniforme(){ //Genera un número uniformemente distribuido en el
+                   //intervalo [0,1) a partir de uno de los generadores
+                   //disponibles en C.
+	double u;
+	  u = (double) rand();
+	  u = u/(double)(RAND_MAX+1.0);
+	return u;
+}
+
+int main(int argc, char * argv[]){
+	if (argc != 2){
+      cerr << "Formato " << argv[0] << " <num_elem>" << endl;
+      return -1;
+    }
+
+  int n = atoi(argv[1]);
+
+  int * T = new int[n];
+  assert(T);
+
+	srand(time(0));
+	double u=uniforme();
+	int p=1+(int)((n-2)*u);
+	T[p]=n-1;
+	for (int i=0; i<p; i++) T[i]=i;
+	for (int i=p+1; i<n; i++) T[i]=n-1-i+p;
+
+	for (int j=0; j<n; j++) {cout << T[j] << " ";}
+
+	cout << "\nFuerza Bruta: " << fuerzaBruta(T,n) << "\n Divide y Vencerás " << divideYVenceras(T,n) <<endl;
+
+	delete T;
 
 }
